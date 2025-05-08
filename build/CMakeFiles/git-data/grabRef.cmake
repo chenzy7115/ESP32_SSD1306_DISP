e7eda26e0300c9f4576ab76c4be2c1315df1f6fa
chenzy7115 <swczy@126.com>
2025-05-08 17:47:52 +0800

    add DHT11

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/ESP32-IDF/display/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/18960/esp/v5.4.1/esp-idf/.git")
+set(GIT_DIR "D:/ESP32-IDF/display/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
--- main/main.c
+++ main/main.c
@@ -3,31 +3,74 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "display.h"
+#include "dht11.h"
 #include "esp_random.h"
+#include <nvs_flash.h>
+#include "esp_log.h"
 
 // 全局定义互斥锁
 static SemaphoreHandle_t lvgl_mutex = NULL;
+// 温度 湿度变量
+int temp = 0, hum = 0;
+
+void dht11_get_valu_task(void *pt)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+    ESP_LOGI(TAG, "[APP] APP Is Start!~\r\n");
+    ESP_LOGI(TAG, "[APP] IDF Version is %d.%d.%d", ESP_IDF_VERSION_MAJOR, ESP_IDF_VERSION_MINOR, ESP_IDF_VERSION_PATCH);
+    ESP_LOGI(TAG, "[APP] Free memory: %lu bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    DHT11_Init(DHT11_GPIO);
+    while (1)
+    {
+        // 获取互斥锁
+        if (xSemaphoreTake(lvgl_mutex, portMAX_DELAY) == pdTRUE)
+        {
+            // 临界区代码，访问共享资源
+            if (DHT11_StartGet(&temp, &hum))
+            {
+                ESP_LOGI(TAG, "temp:%i.%i C     hum:%i%%", temp / 10, temp % 10, hum);
+            }
+
+            // 释放互斥锁
+            xSemaphoreGive(lvgl_mutex);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
 void display_task(void *pt)
 {
-    //需要方法使用的变量采用静态声明，避免在重复调用过程中，占用内存
-    //导致内存溢出的问题。
+    // 需要方法使用的变量采用静态声明，避免在重复调用过程中，占用内存
+    // 导致内存溢出的问题。
     static lv_disp_t *disp;
-
-    lvgl_mutex = xSemaphoreCreateMutex(); // 初始化锁
 
     disp = lvgl_init();
     while (1)
     {
-        xSemaphoreTake(lvgl_mutex, portMAX_DELAY); // 加锁
-        lvgl_display_ui(disp, esp_random() % 101, esp_random() % 101);
-        xSemaphoreGive(lvgl_mutex); // 解锁
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        // 获取互斥锁
+        if (xSemaphoreTake(lvgl_mutex, portMAX_DELAY) == pdTRUE)
+        {
+            // 临界区代码，访问共享资源
+            xSemaphoreTake(lvgl_mutex, portMAX_DELAY); // 加锁
+            lvgl_display_ui(disp, esp_random() % 101, esp_random() % 101);
+            xSemaphoreGive(lvgl_mutex); // 解锁
+            vTaskDelay(pdMS_TO_TICKS(1000));
+
+            // 释放互斥锁
+            xSemaphoreGive(lvgl_mutex);
+        }
     }
 }
 
 void app_main(void)
 {
+    lvgl_mutex = xSemaphoreCreateMutex(); // 初始化锁
+
     // disp = lvgl_init();
     xTaskCreate(display_task, "display", 1024 * 2, NULL, 2, NULL);
     while (1)
